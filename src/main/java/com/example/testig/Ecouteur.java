97eaf72d9fbd5880df9947d1cb6207c93c5414b7
Sami Zouari <sami.zouari@univ-lorraine.fr>
2022-01-27 08:33:02 +0100

    2eme commit test

---
+++ src/main/java/com/example/testig/Ecouteur.java
@@ -0,0 +1,11 @@
+package com.example.testig;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class Ecouteur implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+}
--- src/main/java/com/example/testig/HelloApplication.java
+++ src/main/java/com/example/testig/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.testig;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +12,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+
+
+        stage.setTitle("Titre TP!");
+        BorderPane border = new BorderPane();
+        Button bouton = new Button("Jouer");
+        border.setBottom(bouton);
+        bouton.setOnAction(new Ecouteur());
+        stage.setScene(new Scene(border, 400, 400));
         stage.show();
     }
 
